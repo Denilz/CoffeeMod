952fa22040d0e41aeafe42fc3501f8d0d326f21f
Denilz <ekvdnl@gmail.com>
2024-04-27 01:49:46 +0300

    implemented initial logic

---
+++ CoffeeMod/CoffeeMod/CoffeeModApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeModApp.swift
+//  CoffeeMod
+//
+//  Created by Daniel T on 26/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeModApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
